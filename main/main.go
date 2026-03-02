72d02c81eac5bd8818562192f73176f15095bf71
Daria Maslova <maslovadaria16.07@gmail.com>
2026-03-03 00:45:19 +0300

    second pattern

---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for i := 1; i <= 5; i++ {
+			numbers <- i
+		}
+		close(numbers)
+	}()
+
+	go func() {
+		for n := range numbers {
+			squares <- n * n
+		}
+		close(squares)
+	}()
+
+	for s := range squares { //resulting channel
+		fmt.Println(s)
+	}
+}
+
+//разпределение нагрузки на разные треды
